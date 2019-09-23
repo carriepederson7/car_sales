b4d71c322cf9368ee5649320e911bd02063d59a9
Student <example@example.com>
2019-09-23 15:08:15 -0700

    Add notes to project.

--- car.cs
+++ car.cs
@@ -7,6 +7,7 @@ class Car
   public int Price;
   public int Miles;
 
+  //creates new constructor
   public Car(string makeModel, int price, int miles)
   {
     MakeModel = makeModel;
@@ -14,6 +15,7 @@ class Car
     Miles = miles;
   }
 
+  //function to id if price is less than max
   public bool WorthBuying(int maxPrice)
   {
     return (Price < maxPrice);
@@ -22,6 +24,7 @@ class Car
 
 public class Program
 {
+  //create instances of constructor,
   public static void Main()
   {
     Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
@@ -30,22 +33,27 @@ public class Program
     Car amc = new Car("1976 AMC Pacer", 400, 198000);
 
 
+    //instantiates a built-in constructor List named Car(automobile is the placeholder for the cars in this list)
     List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
 
     Console.WriteLine("Enter maximum price: ");
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
 
+    //creates a new list call CarsMatchingSearch, empty list
     List<Car> CarsMatchingSearch = new List<Car>(0);
 
+    //pushes automobiles to CarsMatchingSearch based on WorthBuying method
     foreach (Car automobile in Cars)
     {
       if (automobile.WorthBuying(maxPrice))
       {
+    //
         CarsMatchingSearch.Add(automobile);
       }
     }
 
+    //writes automobiles in CarsMatchingSerch
     foreach(Car automobile in CarsMatchingSearch)
     {
       Console.WriteLine(automobile.MakeModel);
